9a11ff496c613624779dbe510d46fbdaf5f016c5
notonlynull <daniele.mariotto@gmail.com>
2024-10-20 23:02:41 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Accordion from './accordion/accordion.jsx';
+import './App.css';
+
+function App() {
+  return <Accordion />;
+}
+
+export default App;
---
+++ src/accordion/accordion.jsx
@@ -0,0 +1,28 @@
+const dataAccordion = [
+  {
+    title: 'What is Github and how does it work?',
+    content:
+      'GitHub is the home for all developers—a platform where you can share code, contribute to open source projects, or even automate your workflow with tools like GitHub Actions and Packages. If you’re just getting started with GitHub, you may know us best as a place for version control and collaboration.',
+  },
+  {
+    title: "How do I see GitHub's availability?",
+    content: 'Check our real-time status report',
+  },
+  {
+    title: 'Why is GitHub so popular?',
+    content:
+      'GitHub is built by developers for developers, and we’re proud to be home to the world’s largest open source community. With 50 million developers and millions more open source projects, GitHub has become the go-to place to collaborate and build software together.',
+  },
+];
+
+export default function Accordion() {
+  const [a, setA] = useState(true);
+  const list = dataAccordion.map((i) => (
+    <li>
+      {i.title} <button onClick={setA(!a)}>ABC</button>
+      {a == true && <div>{i.content}</div>}
+    </li>
+  ));
+
+  return <ul>{list}</ul>;
+}
